994779789a1c34178cd7a473cd5c6a396ac07f67
Ubuntu <ubuntu@ip-172-31-1-153.ec2.internal>
2017-02-22 19:58:13 +0000

    locale, logging

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from toodledobot.bot import ToodledoBot
 import os
 import locale
+import logging
 
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+logger = logging.getLogger(__name__)
 
 def main():
-    locale.setlocale(locale.LC_TIME, "ru_RU")
+    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
     bot = ToodledoBot(os.environ['TELEGRAM_TOKEN'])
     bot.start()
 
 if __name__ == '__main__':
+    logger.info("Started...")
     main()
