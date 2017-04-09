da021cc27b23ac2dffa1a9735f8a144c21ae3e91
VDimir <vcherckasov@yandex.ru>
2017-04-09 10:42:32 +0300

    day left in format

--- toodledobot/textformatter.py
+++ toodledobot/textformatter.py
@@ -2,6 +2,7 @@ from toodledoclient.datatypes import Task
 from itertools import count, repeat
 from datetime import date
 
+
 def unlines(arr, elem_str=str):
     return str.join('\n', map(elem_str, arr))
 
@@ -28,14 +29,21 @@ class HtmlTextFormater:
         return tmap.get(tag, '#' + tag)
 
     def due_format(self, duedate):
-        if duedate is None:
-            return ''
         if duedate.year != date.today().year:
             return duedate.strftime("<i>%d %b %Y</i>")
         return duedate.strftime("<i>%d %b, %A</i>")
 
-    def addnl(self, val):
-        return val and '\n'+val
+    def time_delta_format(self, duedate):
+        dt = (duedate - date.today()).days
+        if dt < 0:
+            return "{} days overdue!".format(dt)
+        if dt > 7:
+            return "More than week"
+        if dt == 0:
+            return "Today"
+        if dt == 1:
+            return "Tomorrow"
+        return "{} days left".format(dt)
 
     def tags_format(self, tags):
         return ', '.join(map(self.tag_map, tags))
@@ -46,17 +54,24 @@ class HtmlTextFormater:
         return ''
 
     def prior_format(self, prior):
-        bages = ['\U00002754', '', '\U00002755', '\U0000203C', '\U0001F525']
-        return bages[prior+1]
+        badges = ['\U00002754', '', '\U00002755', '\U0000203C', '\U0001F525']
+        return badges[prior+1]
 
     def task_fmt(self, task=None, num=None):
-        text = str.format("{num}{star}{comp}<b>{title}</b>{prior}{due}{note}{tags}",
-                          num=num or '',
-                          title=task.title,
-                          star=task.star and self.star_sign or '',
-                          comp=task.completed() and self.comp_sign or '',
-                          prior=self.prior_format(task.priority),
-                          due=self.addnl(self.due_format(task.duedate)),
-                          tags=self.addnl(self.tags_format(task.tags)),
-                          note=self.addnl(self.note_format(task.note)))
+        lines = [
+            "{num}{star}{comp}<b>{title}</b>{prior}".format(
+                num=num or '',
+                title=task.title,
+                star=task.star and self.star_sign or '',
+                comp=task.completed() and self.comp_sign or '',
+                prior=self.prior_format(task.priority)),
+            task.duedate and "{due} | {left}".format(
+                due=self.due_format(task.duedate),
+                left=self.time_delta_format(task.duedate)),
+            "{note}".format(note=self.note_format(task.note)),
+            "{tags}".format(tags=self.tags_format(task.tags))
+        ]
+
+        text = '\n'.join(filter(None, lines))
+
         return text
